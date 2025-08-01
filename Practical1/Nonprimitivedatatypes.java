69388669dc1b87e58f44a42e68a5b0743b367943
1112-neha <2420090125@klh.edu.in>
2025-08-01 10:54:36 +0530

    Add files via upload

---
+++ Practical1/Nonprimitivedatatypes.java
@@ -0,0 +1,11 @@
+package Program;
+
+public class Nonprimitivedatatypes {
+
+	public static void main(String[] args) {
+		
+        String greeting = "Hello,World!";
+        System.out.println(greeting);
+	}
+
+}
