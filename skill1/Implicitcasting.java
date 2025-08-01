b748a7fc8eac7f0c734fdb6e269f0d6facc6c52d
1112-neha <2420090125@klh.edu.in>
2025-08-01 15:13:01 +0530

    Add files via upload

---
+++ skill1/Implicitcasting.java
@@ -0,0 +1,17 @@
+package Program;
+
+public class Implicitcasting {
+
+	public static void main(String[] args) {
+		
+        int intNumber = 100;
+        long longNumber;
+
+        longNumber = intNumber;
+
+        System.out.println("Integer value: " + intNumber);
+        System.out.println("Long value after implicit casting: " + longNumber);
+
+	}
+
+}
